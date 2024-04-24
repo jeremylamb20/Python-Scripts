a57ced3feaacd99cd9ee4c8dd02c2ad3833b0593
Jeremy Lamb <jlamb@unc.edu>
2024-04-23 20:07:03 -0400

    new file:   exercises/ex09 - Copy/bear.py 	new file:   exercises/ex09 - Copy/fish.py 	new file:   exercises/ex09 - Copy/river.py 	new file:   exercises/ex09 - Copy/river_simulation.py 	modified:   exercises/ex09/bear.py 	modified:   exercises/ex09/fish.py 	modified:   exercises/ex09/river.py

---
+++ exercises/ex09/bear.py
@@ -0,0 +1,12 @@
+"""File to define Bear class"""
+
+class Bear:
+
+    age: int
+    hunger_score: int
+
+    def __init__(self):
+        return None
+    
+    def one_day(self):
+        return None
